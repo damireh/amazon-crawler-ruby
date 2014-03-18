b58afd1b66a13694f052d0e4118b58c9ded69cef
Daniel Amireh <daniel@amireh.org>
2014-03-18 16:18:14 +0200

    added basic test coverage

--- spec/amazon_spec.rb
+++ spec/amazon_spec.rb
@@ -1,4 +1,12 @@
 require_relative "../amazon"
 
+URL = "http://www.amazon.com/gp/product/0465050654"
+
 describe Amazon do
+  it "accepts an Amazon product URL"
+  it "returns book title"
+  it "returns book author(s)"
+  it "returns book prices (kindle, paperback, etc...)"
+  it "returns all possible ISBN numbers"
+  it "returns book language(s)"
 end
